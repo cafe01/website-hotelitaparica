c644b7f46f6b2dca5ed71356d52feb29daa88021
Carlos Fernando Avila Gratz <cafe01@gmail.com>
2018-10-04 08:44:13 -0300

    initial commit

---
+++ form/reserva.js
@@ -0,0 +1,24 @@
+
+
+exports.fields = [
+    { name: 'name', required: 1 },
+    { name: 'email', required: 1 },
+    { name: 'phone', required: 1 },
+    { name: 'arrival', required: 1 },
+    { name: 'departure', required: 1 },
+    { name: 'adults', required: 1 },
+    { name: 'kids', required: 1 },
+    { type: "recaptcha", key: "6LcwYXMUAAAAAO4xIXLfoLoI7bBxqzDqPiYkCzlb", secret: "6LcwYXMUAAAAAC_wlpWj9-ShHUtBP6tpfjEuyZS3" },
+]
+
+exports.action = function(fields) {
+    var Email = require("k1/email").Email;
+
+    new Email({
+        from: '"Website Hotel Itaparica - Contato" <naoresponder@kreato.com.br>',
+        to: "contato@hotelitaparica.com.br",
+        subject: "Website Hotel Itaparica / Mensagem de: {name}",
+        template: "email/reserva.tt",
+        data: fields
+    }).send()
+}
---
+++ static/js/custom.js
@@ -0,0 +1,184 @@
+
+
+
+$(function() {
+
+    if (!window.google) return;
+
+    // Map Initial Location
+    var initLatitude = -20.3623637;
+    var initLongitude = -40.2972007;
+
+    // Map Markers
+    var mapMarkers = [{
+        // address: "New York, NY 10017",
+        latitude: initLatitude,
+        longitude: initLongitude,
+        icon: {
+            image: "/img/pin.png",
+            iconsize: [26, 46],
+            iconanchor: [12, 46]
+        }
+    }];
+
+    $('#googleMapsMacro').gMap({
+        controls: {
+            draggable: (($.browser.mobile) ? false : true),
+            panControl: true,
+            zoomControl: true,
+            mapTypeControl: true,
+            scaleControl: true,
+            streetViewControl: true,
+            overviewMapControl: true
+        },
+        scrollwheel: false,
+        markers: mapMarkers,
+        latitude: initLatitude,
+        longitude: initLongitude,
+        zoom: 13
+    });
+
+    $('#googleMapsMicro').gMap({
+        controls: {
+            draggable: false,
+            panControl: false,
+            zoomControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            streetViewControl: false,
+            overviewMapControl: false
+        },
+        scrollwheel: false,
+        markers: mapMarkers,
+        latitude: initLatitude,
+        longitude: initLongitude,
+        zoom: 13
+    });
+
+    var mapRef = $('#googleMapsMacro, #googleMapsMicro').data('gMap.reference');
+
+    // Styles from https://snazzymaps.com/
+    var styles = [{
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#e9e9e9"
+        }, {
+            "lightness": 17
+        }]
+    }, {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#f5f5f5"
+        }, {
+            "lightness": 20
+        }]
+    }, {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "color": "#ffffff"
+        }, {
+            "lightness": 17
+        }]
+    }, {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+            "color": "#ffffff"
+        }, {
+            "lightness": 29
+        }, {
+            "weight": 0.2
+        }]
+    }, {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#ffffff"
+        }, {
+            "lightness": 18
+        }]
+    }, {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#ffffff"
+        }, {
+            "lightness": 16
+        }]
+    }, {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#f5f5f5"
+        }, {
+            "lightness": 21
+        }]
+    }, {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#dedede"
+        }, {
+            "lightness": 21
+        }]
+    }, {
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+            "visibility": "on"
+        }, {
+            "color": "#ffffff"
+        }, {
+            "lightness": 16
+        }]
+    }, {
+        "elementType": "labels.text.fill",
+        "stylers": [{
+            "saturation": 36
+        }, {
+            "color": "#333333"
+        }, {
+            "lightness": 40
+        }]
+    }, {
+        "elementType": "labels.icon",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#f2f2f2"
+        }, {
+            "lightness": 19
+        }]
+    }, {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "color": "#fefefe"
+        }, {
+            "lightness": 20
+        }]
+    }, {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+            "color": "#fefefe"
+        }, {
+            "lightness": 17
+        }, {
+            "weight": 1.2
+        }]
+    }];
+
+    var styledMap = new google.maps.StyledMapType(styles, {
+        name: 'Styled Map'
+    });
+
+    mapRef.mapTypes.set('map_style', styledMap);
+    mapRef.setMapTypeId('map_style');
+})
